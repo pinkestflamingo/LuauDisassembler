6e40bb08e7d297bf42a494b465fb14a1bc1d2e1a
Kiko <76973960+KikoTheDon@users.noreply.github.com>
2021-05-26 20:47:19 +0200

    Update dimenzia.lua

--- dimenzia.lua
+++ dimenzia.lua
@@ -1,7 +1,5 @@
 local tokens = {}
 
---; @StackOverflow
---; https://stackoverflow.com/questions/1426954/split-string-in-lua
 tokens.split = function(input, separator)
     if separator == nil then
         separator = "%s"
@@ -47,7 +45,8 @@ local luau_op = {
     FORPREP = "A8",
     CALL = "9F",
     FORLOOP = "8B",
-    RETURN = "82"
+    RETURN = "82",
+    CONCAT = "73"
 }
 local parsedBytes = tokens:Initialize("01 02 05 70 72 69 6E 74 04 77 61 72 6E 01 05 00 00 01 0F A3 00 00 00 8C 02 01 00 8C 00 0A 00 8C 01 01 00 A8 00 09 00 A4 03 01 00 00 00 00 40 52 04 02 00 9F 03 02 01 A4 03 03 00 00 00 20 40 52 04 02 00 9F 03 02 01 8B 00 F7 FF 82 00 01 00 04 03 01 04 00 00 00 40 03 02 04 00 00 20 40 00 00 01 18 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 01 00 00 00 00 00")
 
@@ -155,6 +154,9 @@ for i = 1, Protos do
             [luau_op.FORLOOP] = function()
                 DebugOutput(i2, "FORLOOP", A, B, C, string.format("R[%d] += R[%d + 2]", A, A))
             end,
+            [luau_op.CONCAT] = function()
+                DebugOutput(i2, "CONCAT", A, B, C, string.format("R[%d] = R[%d] .. R[%d]", A, B, C))
+            end,
             [luau_op.RETURN] = function()
                 if B == 1 then 
                     DebugOutput(i2, "RETURN", A, B, C, "return")
@@ -176,7 +178,7 @@ for i = 1, Protos do
                 local ReturnResults = ""
                 -- Not tested
                 if C >= 2 then 
-                    ReturnResults = "R[?] * " .. tostring(C - 1) .. " = "
+                    ReturnResults = string.format("R[%d] to R[%d]", A, A+C-2) .. " = "
                 elseif C == 0 then
                     ReturnResults = string.format("R[%d] = ", LastA + 1)
                 end
